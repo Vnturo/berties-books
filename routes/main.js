ac7d27df06c27c9982c0bd8a8909c1f6f9cd58f0
Victor <victorvrosa@live.com>
2025-12-02 19:24:46 +0000

    Added Weather With all expected changes for 9A.

--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,7 @@
 // Create a new router
 const express = require("express")
 const router = express.Router()
+const request = require('request');
 const redirectLogin = (req, res, next) => {
     if (!req.session.userId ) {
         res.redirect('./login') // redirect to the login page
@@ -17,6 +18,30 @@ router.get('/about',function(req, res, next){
     res.render('about.ejs')
 });
 
+router.get('/weather', function(req, res, next){
+    let city = req.query.city;
+    
+    if (!city) {
+        return res.render('weather.ejs', { weather: null, error: null });
+    }
+    let apiKey = process.env.WEATHER_API_KEY; 
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+    
+    request(url, function (err, response, body) {
+        if(err){
+            next(err);
+        } else {
+            let weather = JSON.parse(body);
+
+            if(weather.main == undefined){
+                res.render('weather.ejs', { weather: null, error: 'Error: No weather data found for that city.'});
+            } else {
+                res.render('weather.ejs', { weather: weather, error: null});
+            }
+        }
+    });
+});
+
 router.get('/logout', redirectLogin, (req,res) => {
         req.session.destroy(err => {
         if (err) {
@@ -26,5 +51,4 @@ router.get('/logout', redirectLogin, (req,res) => {
         })
     })
 
-// Export the router object so index.js can access it
 module.exports = router
